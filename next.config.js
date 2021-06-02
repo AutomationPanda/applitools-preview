58f629dfe5b84a8f7f1b33c8939f67c70e0cf6a9
Colby Fayock <fayock@gmail.com>
2021-06-02 10:33:11 -0400

    target serverless

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const siteConfig = require('./site.config.js');
 const { env } = siteConfig;
 
 module.exports = {
+  target: 'serverless',
   env: {
     NEXT_PUBLIC_GA_PROPERTY_ID: env.gaPropertyId
   }
