9aed4dcdae5f1f430e7ae218b7ba4de5497bef5a
Colby Fayock <fayock@gmail.com>
2021-06-02 09:30:05 -0400

    reducing project deps

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
-const withPlugins = require('next-compose-plugins');
-const images = require('next-images');
 const siteConfig = require('./site.config.js');
 
 const { env } = siteConfig;
 
-module.exports = withPlugins([
-  images
-], {
+module.exports = {
   env: {
     NEXT_PUBLIC_GA_PROPERTY_ID: env.gaPropertyId
   }
-});
+};
